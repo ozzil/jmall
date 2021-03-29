6c73f64901353ba2b880b26a8524a2a6ba0c0f45
wangzilun <zilun.ok@gmail.com>
2021-03-29 15:25:41 +0900

    userManager module

---
+++ jmall-user-manager/src/main/java/com/tokyo/jamll/user/JmallUserManagerApplication.java
@@ -0,0 +1,13 @@
+package com.tokyo.jamll.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmallUserManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmallUserManagerApplication.class, args);
+    }
+
+}
